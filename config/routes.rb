614489bd1b90efb8ea2a00d05c72d26e0086ee1d
duylive <duylive92@gmail.com>
2023-03-14 11:32:00 +0700

    create profile for user

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     resources :users
   end
 
+  get '/profile', to: 'profile#show'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
