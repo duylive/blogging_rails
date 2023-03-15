400896f3f1220cbf226a33e58a3680ce8b5edb27
duylive <duylive92@gmail.com>
2023-03-15 17:22:37 +0700

    fix redirect root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
+  def after_sign_in_path_for(resource)
+    profile_path(resource)
+  end
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
